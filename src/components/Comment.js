d0f8721db991ac8198c23ab18eb547d9dc13f9f6
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-05-30 12:24:53 +0200

    almost done styling

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -37,17 +37,15 @@ export default class Comment extends Component {
           <div style={{marginBottom: 16}}>    
           <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
             <h4> Review: </h4>
-            <div>
-              <button style={{marginRight: 10}} onClick={() => this.props.voteUp(this.props.comment)}> VOTE UP </button> 
-              <button onClick={() => this.props.voteDown(this.props.comment)}> VOTE DOWN </button>
+            <div style={{display: "flex", alignItems: "flex-end", marginBottom: 21}}>
+              <button style={styles.buttonOne} onClick={() => this.props.voteUp(this.props.comment)}>Up</button> 
+              <button style={styles.buttonTwo} onClick={() => this.props.voteDown(this.props.comment)}>Down</button>
+              <span style={{marginBottom: 2}}>{this.props.comment.voteUp - this.props.comment.voteDown}</span>
             </div>  
           </div>
             <p style={styles.commentContent}>{this.props.comment.content}</p>  
           </div>	
         </div>
-        <div style={styles.votesContainer}>
-          <span>{this.props.comment.voteUp - this.props.comment.voteDown}</span>  
-        </div>  
       </div>
   	)
   }
@@ -72,15 +70,34 @@ const styles = {
     flexDirection: "row"
   },
   commentContent: {
+    background: `#e5c572`,
     margin: "0 auto",
     maxWidth: 500, 
-    border: `0.5px solid black`,
-    padding: 12
+    padding: 16,
+    borderRadius: 18
   },
     titleContent: {
+    background: `#e5c572`,
     margin: "0 auto",
+    minWidth: 70,
     maxWidth: 500, 
-    border: `0.5px solid black`,
-    padding: 12
+    padding: 16,
+    borderRadius: 18
+  },
+  buttonOne: {
+    marginRight: 7,
+    width: 42,
+    height: 27,
+    borderRadius: 25,
+    background: `rgb(241, 243, 223)`
+  },
+  buttonTwo: {
+    marginRight: 10,
+    width: 42,
+    height: 27,
+    borderRadius: 25,
+    background: `rgb(241, 243, 223)`
+
   }
+
 }
